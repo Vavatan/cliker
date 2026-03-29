8a45342424c84ac8f19620737ca8ffadfb0f3596
Vavatan <v.a.belarus@gmail.com>
2026-03-29 11:43:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+"use strict"
+
+// ПЕРЕМЕННЫЕ
+let score = 0 // начальный счет
+let scoreByClick = 1 // сколько добавляем за клик
+let scoreBySecond = 0 // сколько добавляем каждую секунду
+let click_sound = new Audio('./click.mp3')
+let bg_sound = new Audio ('./bg.mp3')
+let is_bg = false
+setInterval(addScoreBySecond, 1000)
+
+// ПОЛУЧАЕМ ССЫЛКИ НА ТЕКСТОВЫЕ ПОЛЯ
+const scoreText = document.getElementById('score') // поле со счетом
+const clickText = document.getElementById('add-click') // поле дохода за клик
+const idleText = document.getElementById('add-idle') // поле дохода в секунду
+
+// ПОЛУЧАЕМ ССЫЛКИ НА КНОПКИ И ДОБАВЛЯЕМ СЛУШАТЕЛИ КЛИКА
+const mainButton = document.getElementById('main-button')
+mainButton.onclick = addScoreByClick // addScoreByClick - функция клика
+
+const shopButtons = document.querySelectorAll('.shop') // магазин
+shopButtons.forEach( shopBtn => shopBtn.onclick = getShopClick )
+
+// ФУНКЦИИ
+
+// функция клика
+function addScoreByClick() {
+    click_sound.play()
+    if (is_bg === false) {
+        bg_sound.play() // запускаем
+        is_bg = true // обновляем значения проверки запуска
+    }
+    score = score + scoreByClick // в переменную score обновляем счет
+    scoreText.innerText = score // заменяем текст с очками
+
+    // ЕСЛИ НУЖНО СМЕНИТЬ КАРТИНКУ - добавьте код ниже
+    mainButton.src = './button_on.png' // на какую заменить
+    // какую картинку вернуть и через сколько миллисекунд
+    setTimeout( () => mainButton.src = './button.png', 300)
+}
+
+// добавление очков каждую секунду
+function addScoreBySecond() {
+    score = score + scoreBySecond // в переменную score обновляем счет
+    scoreText.innerText = score // заменяем текст с очками
+}
+
+// клик по карточке в магазине
+function getShopClick(event) {
+    const shop = event.target.closest('.shop')
+    if (!shop) return
+
+    const price = +shop.dataset.price
+    const add = +shop.dataset.add
+    const type = shop.dataset.type
+    if (score < price) {
+        event.target.classList.add('red-border')
+        setTimeout( () => event.target.classList.remove('red-border'), 300 )
+        return
+    }
+    score = score - price
+    scoreText.innerText = score // заменяем текст с очками
+    if (type === "click") {
+        scoreByClick = scoreByClick + add
+        clickText.innerText = scoreByClick
+    } else {
+        scoreBySecond = scoreBySecond + add
+        idleText.innerText = scoreBySecond
+    }
+    shop.classList.add('lime-border')
+    setTimeout( () => shop.classList.remove('lime-border'), 300 )
+}
+
